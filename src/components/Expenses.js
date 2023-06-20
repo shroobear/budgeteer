7454919ad39052a1cc81af09a12e6453c928ec37
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-06-20 11:13:56 -0400

    Imports, renders Expense Table and New Expense Form from their components.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ExpenseTable from "./ExpenseTable";
+import NewExpenseForm from "./NewExpenseForm";
 
 function Expenses() {
+  const [expenses, setExpenses] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    Promise.all([
+      fetch("http://localhost:4000/expenses").then((r) => r.json()),
+      fetch("http://localhost:4000/categories").then((r) => r.json()),
+      fetch("http://localhost:4000/accounts").then((r) => r.json()),
+    ]).then(([expenseData, categoryData, accountData]) => {
+      setExpenses(expenseData);
+      setCategories(categoryData);
+      setAccounts(accountData);
+    });
+    console.log("useEffect rendered ", expenses);
+  }, []);
+
+  function onExpenseAdd(newExpense) {
+    setExpenses([...expenses, newExpense]);
+  }
+
   return (
-    <div>Expenses</div>
-  )
+    <div>
+      <h1>Expenses</h1>
+      <ExpenseTable
+        expenses={expenses}
+        categories={categories}
+        accounts={accounts}
+      />
+      <NewExpenseForm
+        onExpenseAdd={onExpenseAdd}
+        categories={categories}
+        accounts={accounts}
+      />
+    </div>
+  );
 }
 
-export default Expenses
+export default Expenses;
